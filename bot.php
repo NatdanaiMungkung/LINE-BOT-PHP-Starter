fb463293ec82a150d5c4df204370120130a5e011
Natdanai Mungkung <natdanai.mungkung@agoda.com>
2017-04-09 13:05:55 +0700

    Write new userId in text file

--- bot.php
+++ bot.php
@@ -3,44 +3,78 @@ $access_token = 'U5BKa4Ux7/5j8a6uoQBELYHT5DAgXJb9IJ5jyUc3sWSm62AwsgzDZ5oLSbOVW6X
 
 // Get POST body content
 $content = file_get_contents('php://input');
+
+$file_path = 'userId.txt';
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		// Write new only if not unfollow type
+		if ($event['type'] != 'unfollow' && $event['source']['type'] == 'user') {
+			$userId = $event['source']['userId'];
+			$isToWrite = true;
+			$file = fopen($file_path, 'r');
+			if ($file) {
+				while (($line = fgets($handle)) !== false) {
+					// process the line read.
+					if ($line == $userId) {
+						$isToWrite = false;
+						break;
+					}
+						
+				}
+				
+				fclose($file);
+			} else {
+				// error opening the file.
+			} 
+			if ($isToWrite) {
+				$file = fopen($file_path, 'a');
+				fwrite($file, $userId . "\n");
+				fclose($file);
+			}
 			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+			// $text = $event['message']['text'];
+			// // Get replyToken
+			// $replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			// // Build message to reply back
+			// $messages = [
+				// 'type' => 'text',
+				// 'text' => $text
+			// ];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			// if ($event['message']['type'] == 'text') {
+				// // Make a POST Request to Messaging API to reply to sender
+				// $url = 'https://api.line.me/v2/bot/message/reply';
+				// $data = [
+					// 'replyToken' => $replyToken,
+					// 'messages' => [$messages],
+				// ];
+				// $post = json_encode($data);
+				// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+				// $ch = curl_init($url);
+				// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				// $result = curl_exec($ch);
+				// curl_close($ch);
 
-			echo $result . "\r\n";
+				// echo $result . "\r\n";
+			// }
+			
+			
+		}
+		else if ($event['type'] != 'unfollow' && $event['source']['type'] == 'user') {
+			$userId = $event['source']['userId'];
+			$file_contents = file_get_contents($file_path);
+			$file_contents = str_replace($userId . "\n","",$file_contents);
+			file_put_contents($file_path,$file_contents);
 		}
 	}
 }
