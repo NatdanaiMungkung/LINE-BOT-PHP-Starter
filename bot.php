45b66587aaee99cf3398b3f78e110223e13ac5e5
Natdanai Mungkung <natdanai.mungkung@agoda.com>
2017-04-09 13:34:18 +0700

    Update code

--- bot.php
+++ bot.php
@@ -36,34 +36,35 @@ if (!is_null($events['events'])) {
 				fclose($file);
 			}
 			// Get text sent
-			// $text = $event['message']['text'];
+			//$text = $event['message']['text'];
 			// // Get replyToken
 			// $replyToken = $event['replyToken'];
 
-			// // Build message to reply back
-			// $messages = [
-				// 'type' => 'text',
-				// 'text' => $text
-			// ];
+			// build message to reply back
+			$text = 'ขอบคุณครับ เราได้แอดท่านแล้ว เพื่อรับแจ้งเตือนระบบ Truck Tracking'
+			$messages = [
+			'type' => 'text',
+			'text' => $text
+			];
 
 			// if ($event['message']['type'] == 'text') {
 				// // Make a POST Request to Messaging API to reply to sender
-				// $url = 'https://api.line.me/v2/bot/message/reply';
-				// $data = [
-					// 'replyToken' => $replyToken,
-					// 'messages' => [$messages],
-				// ];
-				// $post = json_encode($data);
-				// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				$url = 'https://api.line.me/v2/bot/message/push';
+				$data = [
+				'to' => $userId,
+				'messages' => [$messages],
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-				// $ch = curl_init($url);
-				// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-				// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-				// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-				// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				// $result = curl_exec($ch);
-				// curl_close($ch);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
 
 				// echo $result . "\r\n";
 			// }
