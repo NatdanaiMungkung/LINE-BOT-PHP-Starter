a03424ae79bd87ee5d85000a73a86d3530ea2b53
Natdanai Mungkung <natdanai.mungkung@agoda.com>
2017-04-12 11:42:22 +0700

    Return LineId List

---
+++ LineIdList.php
@@ -0,0 +1,24 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if ($conn->connect_errno) {
+    printf("Connect failed: %s\n", $conn->connect_error);
+    exit();
+}
+
+/* Create table doesn't return a resultset */
+$result = $conn->query("SELECT userId from LineID") {
+  while ($row = $result->fetch_assoc()) {
+      printf ("%s \n", $row["userId"]);
+  }
+}
+
+printf("Hello World.\n");
+?>
